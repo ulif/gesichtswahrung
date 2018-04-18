0172be1d55ebe0a0fb8637bbc1700701d6ab1b10
ulif <uli@gnufix.de>
2018-04-18 17:22:28 +0200

    Start additional face capturing.
    Start record&recognize.

--- facerec_webcam.py
+++ facerec_webcam.py
@@ -54,7 +54,7 @@ def draw_text_box(frame, x, y, text, scale=1.0, width=None, height=None):
     h = height or h
     cv2.rectangle(
         frame, (x, y), (x + w + 5, y + h + 5), (0x00, 0x00, 0x00), cv2.FILLED)
-    cv2.rectangle(frame, (x, y), (x + w + 5, y + h + 5), (0xff, 0xff, 0xff))
+    # cv2.rectangle(frame, (x, y), (x + w + 5, y + h + 5), (0xff, 0xff, 0xff))
     cv2.putText(
         frame, text, (x + 2, y + h + 2), FONT, scale, (0xff, 0xff, 0xff), 1)
 
@@ -84,10 +84,12 @@ faces = Faces()
 found_faces = []
 process_this_frame = True
 mode = 'DETECT'
+picked_face = None
 
 while True:
     # Grab a single frame of video
-    ret, frame = video_capture.read()
+    if mode == 'DETECT':
+        ret, frame = video_capture.read()
 
     # Only process every other frame of video to save time
     if process_this_frame and mode == 'DETECT':
@@ -97,18 +99,38 @@ while True:
     process_this_frame = not process_this_frame
 
     # Display the results
-    for loc, name in found_faces:
-        draw_face_box(frame, name, loc)
+    for num, face in enumerate(found_faces):
+        loc, name = face
+        color = (0x00, 0x00, 0xff)
+        if picked_face == num:
+            color = (0x00, 0xff, 0xff)
+        draw_face_box(frame, name, loc, color=color)
 
     draw_text_box(frame, 0, 0, "MODE: %s" % mode)
-    draw_text_box(frame, 0, 62, "<press q to quit>", 0.6)
+    bottom = frame.shape[0]
+    draw_text_box(frame, 0, bottom - 21,
+        "<press q to quit, s to switch mode>", 0.6)
+    if mode == 'SNAPSHOT':
+        draw_text_box(frame, 0, 25, 'SPC to select, ENTER to choose', 0.6)
+
 
     # Display the resulting image
     cv2.imshow('Video', frame)
 
     # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xff == ord('q'):
+    key = cv2.waitKey(1) & 0xff
+    if key == ord('q'):
         break
+    elif key == ord('s'):
+        if mode == 'DETECT':
+            mode = 'SNAPSHOT'
+            picked_face = 0
+        else:
+            mode = 'DETECT'
+            picked_face = None
+    elif key == ord(' '):
+        if mode == 'SNAPSHOT' and len(found_faces):
+            picked_face = (picked_face + 1) % len(found_faces)
 
 # Release handle to the webcam
 video_capture.release()
