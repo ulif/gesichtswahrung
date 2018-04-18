85142f5d50939fb1e02980d0c587eef4aba24633
ulif <uli@gnufix.de>
2018-04-18 10:18:21 +0200

    Add function to draw boxes around faces.
    The boxes provide a name and a frame. The color used can be determined
    on call.

--- facerec_webcam.py
+++ facerec_webcam.py
@@ -59,6 +59,19 @@ def draw_text_box(frame, x, y, text, scale=1.0, width=None, height=None):
         frame, text, (x + 2, y + h + 2), FONT, scale, (0xff, 0xff, 0xff), 1)
 
 
+def draw_face_box(frame, name, loc, color=(0x00, 0x00, 0xff)):
+    """Draw a box on `frame` at location `loc`.
+
+    Draws a little frame around faces on location `loc`, couloured in `color`
+    (BGR model). The name `name` is put into the box bottom, like a caption.
+    """
+    top, right, bottom, left = loc
+    cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
+    cv2.rectangle(
+        frame, (left, bottom - 35), (right, bottom), color, cv2.FILLED)
+    cv2.putText(
+        frame, name, (left + 6, bottom - 6), FONT, 1.0, (0xff, 0xff, 0xff), 1)
+
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
@@ -81,17 +94,8 @@ while True:
     process_this_frame = not process_this_frame
 
     # Display the results
-    for (top, right, bottom, left), name in found_faces:
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-        # Draw a label with a name below the face
-        cv2.rectangle(
-                frame, (left, bottom - 35), (right, bottom),
-                (0x00, 0x00, 0xff), cv2.FILLED)
-        cv2.putText(
-                frame, name, (left + 6, bottom - 6),
-                FONT, 1.0, (0xff, 0xff, 0xff), 1)
+    for loc, name in found_faces:
+        draw_face_box(frame, name, loc)
 
     draw_text_box(frame, 0, 0, "MODE: %s" % mode)
     draw_text_box(frame, 0, 62, "<press q to quit>", 0.6)
